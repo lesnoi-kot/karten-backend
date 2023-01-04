4cc2bd761d61be16f1c62de9f800d1310cfd3e43
yuko <yuriii.kovalyov@gmail.com>
2023-01-04 18:07:25 +0300

    Finished tests for projects controllers

--- go.mod
+++ go.mod
@@ -9,14 +9,19 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/time v0.0.0-20201208040808-7e3f01d25324 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
--- src/api/projects_test.go
+++ src/api/projects_test.go
@@ -1,6 +1,242 @@
-package api
+package api_test
 
-import "testing"
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
 
-func TestGetProjects(t *testing.T) {
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+	"go.uber.org/zap"
+
+	"github.com/lesnoi-kot/karten-backend/src/api"
+	"github.com/lesnoi-kot/karten-backend/src/store"
+)
+
+type mockProjectsStore struct {
+	mock.Mock
+}
+
+type projectsSuite struct {
+	suite.Suite
+	store *store.Store
+	api   *api.APIService
+
+	projectsMock *mockProjectsStore
+}
+
+func (suite *projectsSuite) SetupTest() {
+	suite.projectsMock = new(mockProjectsStore)
+	suite.store = &store.Store{
+		DB:       nil,
+		Projects: suite.projectsMock,
+		Boards:   nil,
+	}
+	suite.api = api.NewAPI(api.APIConfig{
+		Store:     suite.store,
+		Logger:    zap.NewNop().Sugar(),
+		APIPrefix: "",
+	})
+}
+
+func (s *projectsSuite) TestGetProjects() {
+	s.Run("200", func() {
+		req := httptest.NewRequest(http.MethodGet, "/projects", nil)
+		rec := httptest.NewRecorder()
+
+		s.projectsMock.On("GetAll").Return(
+			[]*store.Project{
+				{ID: "1", Name: "A"},
+				{ID: "2", Name: "B"},
+				{ID: "3", Name: "C"},
+			},
+			nil,
+		).Once()
+		s.api.Server().Handler.ServeHTTP(rec, req)
+
+		s.Equal(http.StatusOK, rec.Code)
+		s.JSONEq(`{
+			"error": null,
+			"data": [
+				{"id": "1", "name": "A"},
+				{"id": "2", "name": "B"},
+				{"id": "3", "name": "C"}
+			]
+		}`, rec.Body.String())
+	})
+
+	s.Run("Error from the store", func() {
+		req := httptest.NewRequest(http.MethodGet, "/projects", nil)
+		rec := httptest.NewRecorder()
+		s.projectsMock.On("GetAll").Return([]*store.Project(nil), errors.New("BOOM")).Once()
+		s.api.Server().Handler.ServeHTTP(rec, req)
+		s.Equal(http.StatusInternalServerError, rec.Code)
+	})
+}
+
+func (s *projectsSuite) TestGetProject() {
+	s.Run("200", func() {
+		req := httptest.NewRequest(http.MethodGet, "/projects/id-1", nil)
+		rec := httptest.NewRecorder()
+
+		s.projectsMock.On("Get", "id-1").Return(
+			&store.Project{ID: "id-1", Name: "First"},
+			nil,
+		).Once()
+		s.api.Server().Handler.ServeHTTP(rec, req)
+
+		s.Equal(http.StatusOK, rec.Code)
+		s.JSONEq(`{
+			"error": null,
+			"data": {"id": "id-1", "name": "First"}
+		}`, rec.Body.String())
+	})
+
+	s.Run("404", func() {
+		req := httptest.NewRequest(http.MethodGet, "/projects/id-not-found", nil)
+		rec := httptest.NewRecorder()
+		s.projectsMock.On("Get", "id-not-found").Return(
+			(*store.Project)(nil),
+			store.ErrNotFound,
+		).Once()
+		s.api.Server().Handler.ServeHTTP(rec, req)
+
+		s.Equal(http.StatusNotFound, rec.Code)
+	})
+
+	s.Run("Invalid path", func() {
+		req := httptest.NewRequest(http.MethodGet, "/projects/gfgd/fdgfgdf/g", nil)
+		rec := httptest.NewRecorder()
+		s.projectsMock.On("Get").Return(
+			(*store.Project)(nil),
+			store.ErrNotFound,
+		).Once()
+		s.api.Server().Handler.ServeHTTP(rec, req)
+
+		s.Equal(http.StatusNotFound, rec.Code)
+	})
+}
+
+func (s *projectsSuite) TestAddProject() {
+	s.Run("Valid input", func() {
+		req := httptest.NewRequest(
+			http.MethodPost,
+			"/projects",
+			strings.NewReader(`{"name":"Unit-test"}`),
+		)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+
+		s.projectsMock.On("Add", "Unit-test").Return(
+			&store.Project{ID: "new-id", Name: "Unit-test"},
+			nil,
+		).Once()
+		s.api.Server().Handler.ServeHTTP(rec, req)
+
+		s.Equal(http.StatusOK, rec.Code)
+		s.JSONEq(`{
+			"error": null,
+			"data": {"id": "new-id", "name": "Unit-test"}
+		}`, rec.Body.String())
+	})
+
+	s.Run("Invalid input", func() {
+		req := httptest.NewRequest(
+			http.MethodPost,
+			"/projects",
+			strings.NewReader(`{"name":""}`),
+		)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		s.api.Server().Handler.ServeHTTP(rec, req)
+		s.Equal(http.StatusBadRequest, rec.Code)
+	})
+}
+
+func (s *projectsSuite) TestDeleteProject() {
+	s.Run("200", func() {
+		req := httptest.NewRequest(http.MethodDelete, "/projects/111", nil)
+		rec := httptest.NewRecorder()
+		s.projectsMock.On("Delete", "111").Return(nil).Once()
+		s.api.Server().Handler.ServeHTTP(rec, req)
+		s.Equal(http.StatusNoContent, rec.Code)
+	})
+
+	s.Run("404", func() {
+		req := httptest.NewRequest(http.MethodDelete, "/projects/666", nil)
+		rec := httptest.NewRecorder()
+		s.projectsMock.On("Delete", "666").Return(store.ErrNotFound).Once()
+		s.api.Server().Handler.ServeHTTP(rec, req)
+		s.Equal(http.StatusNotFound, rec.Code)
+	})
+}
+
+func (s *projectsSuite) TestEditProject() {
+	s.Run("200", func() {
+		req := httptest.NewRequest(http.MethodPatch, "/projects/111", strings.NewReader(`{"name":"New"}`))
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		s.projectsMock.
+			On("Edit", store.EditProjectArgs{ID: "111", Name: "New"}).
+			Return(&store.Project{ID: "111", Name: "New"}, nil).
+			Once()
+		s.api.Server().Handler.ServeHTTP(rec, req)
+		s.Equal(http.StatusOK, rec.Code)
+		s.JSONEq(`{
+			"error": null,
+			"data": {"id": "111", "name": "New"}
+		}`, rec.Body.String())
+	})
+
+	s.Run("400", func() {
+		req := httptest.NewRequest(http.MethodPatch, "/projects/111", strings.NewReader(`{"name":""}`))
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		s.api.Server().Handler.ServeHTTP(rec, req)
+		s.Equal(http.StatusBadRequest, rec.Code)
+	})
+
+	s.Run("400", func() {
+		req := httptest.NewRequest(http.MethodPatch, "/projects/111", strings.NewReader(`{"name":"x"}`))
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		s.projectsMock.
+			On("Edit", store.EditProjectArgs{ID: "111", Name: "x"}).
+			Return((*store.Project)(nil), store.ErrNotFound).
+			Once()
+		s.api.Server().Handler.ServeHTTP(rec, req)
+		s.Equal(http.StatusNotFound, rec.Code)
+	})
+}
+
+func TestProjects(t *testing.T) {
+	suite.Run(t, new(projectsSuite))
+}
+
+func (m mockProjectsStore) Get(id string) (*store.Project, error) {
+	args := m.Called(id)
+	return args.Get(0).(*store.Project), args.Error(1)
+}
+
+func (m mockProjectsStore) GetAll() ([]*store.Project, error) {
+	args := m.Called()
+	return args.Get(0).([]*store.Project), args.Error(1)
+}
+
+func (m mockProjectsStore) Add(name string) (*store.Project, error) {
+	args := m.Called(name)
+	return args.Get(0).(*store.Project), args.Error(1)
+}
+
+func (m mockProjectsStore) Edit(a store.EditProjectArgs) (*store.Project, error) {
+	args := m.Called(a)
+	return args.Get(0).(*store.Project), args.Error(1)
+}
+
+func (m mockProjectsStore) Delete(id string) error {
+	args := m.Called(id)
+	return args.Error(0)
 }
